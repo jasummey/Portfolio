892cb67d85518bfb1e4773e59a8d5c720d37f25c
Jane Summey <beatrice.summey@mykenzie.snhu.edu>
2026-03-22 19:21:33 -0400

    Update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "../components/styles/home.css";
 // import profilePic from "./profilepic.jpg";
-import newprofilepicdec2023 from "./newprofilepicdec2023.jpg";
+import headshot from "./headshot.jpg";
 
 function Home() {
   return (
     <div className="home">
-      <img
-        className="img"
-        src={newprofilepicdec2023}
-        alt="Jane Summey headshot"
-      ></img>
+      <img className="img" src={headshot} alt="Jane Summey headshot"></img>
 
       <div className="about">
         <h2>Nice to meet you, my name is Jane</h2>
         <div className="prompt">
           <span>
-            I'm a new web developer with a passion for learning new things and
-            growing in the field of web development. My interest began when I
-            first learned the endless possibilities you can accomplish with
-            writing code and designing web applications. When I am not coding or
-            working, I am leading and teaching a group of Cub Scouts, as a Den
-            Leader, for the Boy Scouts of America.
+            Data-focused Business Intelligence professional with hands-on
+            experience delivering analytics and reporting solutions using Power
+            BI, Excel, and Python, supported by formal coursework in SQL and
+            data modeling. Strong foundation in ETL concepts, dimensional
+            modeling, data quality practices, and stakeholder collaboration.
+            Experienced in transforming raw data into reliable, well-documented
+            datasets that support executive decision-making and scalable
+            self-service analytics.
           </span>
         </div>
       </div>
